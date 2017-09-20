27b2101a4d5cbe4e29f351f004dc4881e9f5612e
NJun <natashajuneja@gmail.com>
2017-09-19 19:31:07 -0500

    feat: Organic Online Shop for buying produce
    Added Angular framework for an online 
    shop and integrated it with Bootstrap and Firebase and also added the
    Navigaion Bar component.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAataBa0ws8HBEpBnns-H1hlZIOJldTt44",
+    authDomain: "organicshop-eb629.firebaseapp.com",
+    databaseURL: "https://organicshop-eb629.firebaseio.com",
+    projectId: "organicshop-eb629",
+    storageBucket: "organicshop-eb629.appspot.com",
+    messagingSenderId: "1014364867151"
+
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+        apiKey: "AIzaSyAataBa0ws8HBEpBnns-H1hlZIOJldTt44",
+        authDomain: "organicshop-eb629.firebaseapp.com",
+        databaseURL: "https://organicshop-eb629.firebaseio.com",
+        projectId: "organicshop-eb629",
+        storageBucket: "organicshop-eb629.appspot.com",
+        messagingSenderId: "1014364867151"
+  }
 };
